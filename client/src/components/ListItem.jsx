971b7dc373a9b74f23f9c772fa49dd61a1588694
YHJ96 <yhj878812@naver.com>
2022-06-22 23:39:35 +0900

    feat: 리스트 아이템 컴포넌트 생성

---
+++ client/src/components/ListItem.jsx
@@ -0,0 +1,26 @@
+import React from 'react';
+import { Card } from 'react-bootstrap';
+import propTypes from 'prop-types';
+
+function ListItem({ title, content, color }) {
+    return (
+        <Card>
+            <Card.Body>
+                <Card.Title>{title}</Card.Title>
+                <Card.Text>{content}</Card.Text>
+            </Card.Body>
+        </Card>
+    )
+}
+
+ListItem.defaultProps = {
+    title: "글 제목",
+    content: "글 내용"
+}
+
+ListItem.propTypes = {
+    title: propTypes.string,
+    content: propTypes.string
+}
+
+export default ListItem;
