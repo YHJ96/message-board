fee5f374b74b0c19b25b2aef79f7aba9c3aec225
YHJ96 <yhj878812@naver.com>
2022-06-22 23:40:48 +0900

    feat: 리스트 라우터 경로 설정

--- server/app.js
+++ server/app.js
@@ -2,15 +2,22 @@ const express = require("express");
 const path = require("path");
 // const cors = require("cors");
 const app = express();
-const PORT = 5000;
+const PORT = 3333;
 const createPath = (fileURL = '') => path.join(__dirname, `../client/build`, fileURL);
+
+const data =  [
+  { title: "양희준", content: "코드스테이츠" },
+  { title: "심소영", content: "코드스테이츠" },
+  { title: "이유진", content: "코드스테이츠" }];
 
 app.use(express.json());
 app.use(express.static(createPath()));
 
+app.get('/', (req, res) => res.sendFile(createPath("/index.html")));
+// app.get('*', (req, res) => res.sendFile(createPath("/index.html")));
+
+app.get('/list', (req, res) => res.status(200).json(data));
+
 app.listen(PORT, () => {
   console.log("서버 작동중");
 });
-
-app.get('/', (req, res) => res.sendFile(createPath("/index.html")));
-app.get('*', (req, res) => res.sendFile(createPath("/index.html")));
